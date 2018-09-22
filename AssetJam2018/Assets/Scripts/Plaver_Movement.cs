60c3bed59e476b8a7bf1782969910b7f16d214c2
DoctorOum <43484827+DoctorOum@users.noreply.github.com>
2018-09-22 01:38:37 -0400

    Added code for movement

---
+++ AssetJam2018/Assets/Scripts/Plaver_Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plaver_Movement : MonoBehaviour
+{
+
+
+    private Rigidbody2D myRigidbody;
+
+    [SerializeField]
+    private float movementSpeed;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        HandleMovement(horizontal);
+    }
+    private void HandleMovement(float horizontal)
+    {
+
+        myRigidbody.velocity = new Vector2(horizontal * movementSpeed, myRigidbody.velocity.y);
+    }
+}
+
