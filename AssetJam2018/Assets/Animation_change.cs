f6d1d3d4728581a46332c51e31a5483bdbb32d32
GOODSPIDERR <43484800+GOODSPIDERR@users.noreply.github.com>
2018-09-22 02:36:57 -0400

    Revert "glowy boi"
    This reverts commit 8113d5a34972fe7889423e61eda4670e7c00d4a3.

---
+++ AssetJam2018/Assets/Animation_change.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation_change : StateMachineBehaviour {
+
+	public GameObject player;
+
+	public override void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+	{
+
+	}
+
+	
+}
