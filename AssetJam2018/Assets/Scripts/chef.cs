dc4076ae8ea17ec900bb3eb6409e7a8ec38e8ba0
robbytaku <43484817+robbytaku@users.noreply.github.com>
2018-09-22 19:44:06 -0400

    here

--- AssetJam2018/Assets/Scripts/chef.cs
+++ AssetJam2018/Assets/Scripts/chef.cs
@@ -2,18 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class chef : MonoBehaviour {
+public class Chef : MonoBehaviour
+   {
+    public float walkSpeed = 1.0f;      // Walkspeed
+    public float wallLeft = 0.0f;       // Define wallLeft
+    public float wallRight = 5.0f;      // Define wallRight
+    float walkingDirection = 1.0f;
+    Vector2 walkAmount;
+    float originalX; // Original float value
 
-    public Transform[] patrolPoints;
-    public float speed;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.Translate(Vector3.right * Time.deltaTime * speed);
-	}
+    void Start()
+    {
+    this.originalX = this.transform.position.x;
+    }
+
+    // Update is called once per frame
+        void Update()
+        {
+        walkAmount.x = walkingDirection * walkSpeed * Time.deltaTime;
+        if (walkingDirection > 0.0f && transform.position.x >= originalX + wallRight)
+        {
+            walkingDirection = -1.0f;
+        }
+        else if (walkingDirection < 0.0f && transform.position.x <= originalX - wallLeft)
+        {
+            walkingDirection = 1.0f;
+        }
+        transform.Translate(walkAmount);
+    }
 }
