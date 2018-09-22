063b09ea0293edcc0ace1c4143892ecffe842dbf
GOODSPIDERR <43484800+GOODSPIDERR@users.noreply.github.com>
2018-09-22 02:06:04 -0400

    shipped

--- AssetJam2018/Assets/Scripts/Position.cs
+++ AssetJam2018/Assets/Scripts/Position.cs
@@ -15,9 +15,9 @@ public class Position : MonoBehaviour {
 	void LateUpdate () {
 		transform.position = player.transform.position + offset;
 
-		if (player.transform.position.x <= -3.45f)
-			transform.position = new Vector3(-3.45f, transform.position.y, transform.position.z);
-		if (player.transform.position.x >= 6.05f)
-			transform.position = new Vector3(6.05f, transform.position.y, transform.position.z);
+		if (player.transform.position.x <= -3.3f)
+			transform.position = new Vector3(-3.3f, transform.position.y, transform.position.z);
+		if (player.transform.position.x >= 5.85f)
+			transform.position = new Vector3(5.85f, transform.position.y, transform.position.z);
 	}
 }
