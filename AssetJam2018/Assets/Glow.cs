6b8a6e75c45a39565e55a9980dc3ffe96adb1b69
GOODSPIDERR <43484800+GOODSPIDERR@users.noreply.github.com>
2018-09-22 15:16:05 -0400

    weapon changes

--- AssetJam2018/Assets/Glow.cs
+++ AssetJam2018/Assets/Glow.cs
@@ -11,13 +11,22 @@ public class Glow : MonoBehaviour
     public Transform player;
     public float leftEdge;
     public float rightEdge;
+    private bool interact;
+    public GameObject weapon;
 
     private void Update()
     {
         if (player.position.x >= leftEdge && player.position.x <= rightEdge)
-            spriteRenderer.sprite = spriteGlow;
+            interact = true;
+        
 
         if (player.position.x < leftEdge || player.position.x > rightEdge)
+            interact = false;
+
+        if (interact == true)
+            spriteRenderer.sprite = spriteGlow;
+
+        if (interact == false)
             spriteRenderer.sprite = sprite;
     }
 
