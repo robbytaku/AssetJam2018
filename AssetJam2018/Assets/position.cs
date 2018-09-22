35dee1776bf50acab36c34bf63d55840b2136fdd
GOODSPIDERR <43484800+GOODSPIDERR@users.noreply.github.com>
2018-09-22 01:51:09 -0400

    camera working woo

---
+++ AssetJam2018/Assets/position.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class position : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	void FixedUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
