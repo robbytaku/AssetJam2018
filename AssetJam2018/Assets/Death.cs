073030a5c015db24dca7a9d926dcf692ff9945ab
GOODSPIDERR <43484800+GOODSPIDERR@users.noreply.github.com>
2018-09-23 02:26:27 -0400

    Revert "whatever"
    This reverts commit ea621269db3062c0ec1231307b8a4a853b4025b3.

--- AssetJam2018/Assets/Death.cs
+++ AssetJam2018/Assets/Death.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class Death : MonoBehaviour {
 
 	public GameObject meat;
-
+<<<<<<< HEAD
+=======
 	public AudioSource screamSound;
+>>>>>>> a76ff8f19a2788b46cb0d514b697e3f4c83af39a
 
-
+	// Use this for initialization
 	void Start () {
 		
 	}
 
+	// Update is called once per frame
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
+<<<<<<< HEAD
+=======
 		screamSound.Play();
+>>>>>>> a76ff8f19a2788b46cb0d514b697e3f4c83af39a
 		Destroy(gameObject);
 		Instantiate(meat, transform.position - (transform.up * 0.15f), transform.rotation);
 	}
