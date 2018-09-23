3ec80e21ba48117e966d7598aa17be77397d073c
GOODSPIDERR <43484800+GOODSPIDERR@users.noreply.github.com>
2018-09-22 20:13:42 -0400

    fixed animations

--- AssetJam2018/Assets/Scripts/Glow.cs
+++ AssetJam2018/Assets/Scripts/Glow.cs
@@ -14,7 +14,7 @@ public class Glow : MonoBehaviour
     public float leftEdge;
     public float rightEdge;
     private bool interact;
-    public GameObject weapon;
+    public int weapon;
     public BoxCollider2D playerCollider;
     public bool unlocked = true;
     public bool door = false;
@@ -23,48 +23,66 @@ public class Glow : MonoBehaviour
     private void Update()
     {
 
-		if (player.position.x >= leftEdge && player.position.x <= rightEdge && hidden == false)
-			interact = true;
+        if (player.position.x >= leftEdge && player.position.x <= rightEdge && hidden == false)
+            interact = true;
 
-		if (player.position.x < leftEdge || player.position.x > rightEdge && hidden == false)
-			interact = false;
+        if (player.position.x < leftEdge || player.position.x > rightEdge && hidden == false)
+            interact = false;
 
 
-		if (interact == false && hidden == true)
-		{
-			spriteRenderer.sprite = sprite;
-			controller.movementSpeed = 0f;
-			if (Input.GetKeyDown("w") || (Input.GetKeyDown("up")))
-			{
-				hidden = false;
-				playerSprite.enabled = true;
-				playerCollider.enabled = true;
-				controller.movementSpeed = 1f;
-			}
-		}
+        if (interact == false && hidden == true)
+        {
+            spriteRenderer.sprite = sprite;
+            controller.movementSpeed = 0f;
+            if (Input.GetKeyDown("w") || (Input.GetKeyDown("up")))
+            {
+                hidden = false;
+                playerSprite.enabled = true;
+                playerCollider.enabled = true;
+                controller.movementSpeed = 1f;
 
-		if (interact == true && hidden == false) {
+            }
+        }
+
+        if (interact == true && hidden == false)
+        {
             spriteRenderer.sprite = spriteGlow;
             if (Input.GetKeyDown("w") || (Input.GetKeyDown("up")))
+            {
+                if (weapon == 1 && controller.weapon == 0)
+                    controller.weapon = 1;
+                if (weapon == 2 && controller.weapon == 0)
+                    controller.weapon = 2;
+                if (weapon == 3 && controller.weapon == 0)
+                    controller.weapon = 3;
+                if (weapon == 4 && controller.weapon == 0)
+                    controller.weapon = 4;
+                if (weapon == 5 && controller.weapon == 0)
+                    controller.weapon = 5;
+
                 if (door == true && unlocked == true)
-				{
-                   hidden = true;
-                   playerSprite.enabled = false;
-                   playerCollider.enabled = false;
-					interact = false;
+                {
+                    hidden = true;
+                    playerSprite.enabled = false;
+                    playerCollider.enabled = false;
+                    interact = false;
+
+
+
                 }
-		
 
-		}
+            }
 
-        if (interact == false) {
+        }
+
+        if (interact == false)
             spriteRenderer.sprite = sprite;
 
-		
-
-		}
-        
     }
-
 }
+
+		
+        
+    
+
 
