32d7f49fd762cf6f8f362fb8dce94de94de7ddc2
GOODSPIDERR <43484800+GOODSPIDERR@users.noreply.github.com>
2018-09-23 02:26:25 -0400

    Revert "Revert "Merge branch 'master' of https://github.com/robbytaku/AssetJam2018""
    This reverts commit ee0fa0989b398abdfbae8ae036232cbac5fd2a84.

--- AssetJam2018/Assets/Death.cs
+++ AssetJam2018/Assets/Death.cs
@@ -5,10 +5,7 @@ using UnityEngine;
 public class Death : MonoBehaviour {
 
 	public GameObject meat;
-<<<<<<< HEAD
 
-=======
->>>>>>> parent of 040043b... Merge branch 'master' of https://github.com/robbytaku/AssetJam2018
 	public AudioSource screamSound;
 
 
