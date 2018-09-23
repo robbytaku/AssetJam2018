3767da6c85807790a38a4bb72284ce97a803ebd5
GOODSPIDERR <43484800+GOODSPIDERR@users.noreply.github.com>
2018-09-23 14:27:07 -0400

    fixed the tumours

--- AssetJam2018/Assets/Death.cs
+++ AssetJam2018/Assets/Death.cs
@@ -5,52 +5,11 @@ using UnityEngine;
 public class Death : MonoBehaviour {
 
 	public GameObject meat;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> parent of ec039bd... Revert "Revert "Merge branch 'master' of https://github.com/robbytaku/AssetJam2018""
-=======
 	public AudioSource screamSound;
->>>>>>> a76ff8f19a2788b46cb0d514b697e3f4c83af39a
-=======
->>>>>>> parent of a76ff8f... asd
-=======
->>>>>>> parent of a76ff8f... asd
-=======
 	public AudioSource screamSound;
->>>>>>> ddc3bc55b16cb7dc0f341d145c5d53398117e471
-<<<<<<< HEAD
-=======
-=======
 	public AudioSource screamSound;
->>>>>>> a76ff8f19a2788b46cb0d514b697e3f4c83af39a
->>>>>>> parent of ddc3bc5... Revert "Merge branch 'master' of https://github.com/robbytaku/AssetJam2018"
-=======
 	public AudioSource screamSound, soundSmash;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 	public GameObject slash;
->>>>>>> c95f10b5acdbb195fffc8cb55b39bc1e812b08f5
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of ec039bd... Revert "Revert "Merge branch 'master' of https://github.com/robbytaku/AssetJam2018""
-=======
->>>>>>> parent of c95f10b... good game bois
->>>>>>> parent of 4625f86... Revert "Revert "good game bois""
-=======
->>>>>>> parent of c95f10b... good game bois
->>>>>>> parent of 4625f86... Revert "Revert "good game bois""
-=======
->>>>>>> parent of c95f10b... good game bois
-=======
->>>>>>> parent of c95f10b... good game bois
 
 	// Use this for initialization
 	void Start () {
@@ -60,28 +19,10 @@ public class Death : MonoBehaviour {
 	// Update is called once per frame
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> parent of ec039bd... Revert "Revert "Merge branch 'master' of https://github.com/robbytaku/AssetJam2018""
-=======
 		screamSound.Play();
->>>>>>> a76ff8f19a2788b46cb0d514b697e3f4c83af39a
-=======
->>>>>>> parent of a76ff8f... asd
-=======
->>>>>>> parent of a76ff8f... asd
-=======
 		screamSound.Play();
->>>>>>> ddc3bc55b16cb7dc0f341d145c5d53398117e471
 		Destroy(gameObject);
 		Instantiate(meat, transform.position - (transform.up * 0.15f), transform.rotation);
-=======
 		if(collision.gameObject.tag.Equals("Projectile"))
 		{ 
 			screamSound.Play();
@@ -95,6 +36,5 @@ public class Death : MonoBehaviour {
 			Destroy(gameObject);
 			Instantiate(meat, transform.position - (transform.up * 0.15f), transform.rotation);
 		}
->>>>>>> c95f10b5acdbb195fffc8cb55b39bc1e812b08f5
 	}
 }
