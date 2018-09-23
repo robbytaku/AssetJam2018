ddc3bc55b16cb7dc0f341d145c5d53398117e471
GOODSPIDERR <43484800+GOODSPIDERR@users.noreply.github.com>
2018-09-23 02:26:28 -0400

    Revert "Merge branch 'master' of https://github.com/robbytaku/AssetJam2018"
    This reverts commit 040043beb7a61033709c8aea3783f9e4c2e8aefc, reversing
    changes made to 3c6f175b7ed2abc95fa9ba34ee7eb0ed842e72e6.

--- AssetJam2018/Assets/Death.cs
+++ AssetJam2018/Assets/Death.cs
@@ -5,10 +5,7 @@ using UnityEngine;
 public class Death : MonoBehaviour {
 
 	public GameObject meat;
-<<<<<<< HEAD
-=======
 	public AudioSource screamSound;
->>>>>>> a76ff8f19a2788b46cb0d514b697e3f4c83af39a
 
 	// Use this for initialization
 	void Start () {
@@ -18,10 +15,7 @@ public class Death : MonoBehaviour {
 	// Update is called once per frame
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
-<<<<<<< HEAD
-=======
 		screamSound.Play();
->>>>>>> a76ff8f19a2788b46cb0d514b697e3f4c83af39a
 		Destroy(gameObject);
 		Instantiate(meat, transform.position - (transform.up * 0.15f), transform.rotation);
 	}
